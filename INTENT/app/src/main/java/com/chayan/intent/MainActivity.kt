cf006e6cc6c91b192d1ed7037481f7393287220c
Chayan Chandro Sutrodhar <chayansd5656@gmail.com>
2023-04-03 21:37:23 +0600

    explicit Intent

---
+++ INTENT/app/src/main/java/com/chayan/intent/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.chayan.intent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+
+            intent = Intent(applicationContext,MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
