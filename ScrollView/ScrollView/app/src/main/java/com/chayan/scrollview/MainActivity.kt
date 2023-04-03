0645ab5260348009825c035d9927b7951e9a7bc0
Chayan Chandro Sutrodhar <109588297+ChayanSD@users.noreply.github.com>
2023-04-03 22:33:32 +0600

    Add files via upload

---
+++ ScrollView/ScrollView/app/src/main/java/com/chayan/scrollview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.chayan.scrollview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
