bdc8763791dae3c17b43bb1b99fda928e506731c
Chayan Chandro Sutrodhar <109588297+ChayanSD@users.noreply.github.com>
2023-04-07 21:28:28 +0600

    Add files via upload

---
+++ WevView/app/src/main/java/com/chayan/wevview/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.chayan.wevview
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val webView = findViewById<WebView>(R.id.webview)
+        webViewSetUp(webView)
+    }
+ @RequiresApi(Build.VERSION_CODES.O)
+ private fun webViewSetUp(webView: WebView){
+            webView.webViewClient= WebViewClient()
+            webView.apply {
+                settings.javaScriptEnabled=true
+                settings.safeBrowsingEnabled=true
+                loadUrl("https://chayansd.hashnode.dev/")
+            }
+
+    }
+}
